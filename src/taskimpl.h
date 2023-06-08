96a7ef04639bedbe2750fd99e0c324cb28774999
Lawrence Stubbs <lstubbs@techno.express>
2023-06-07 21:13:35 -0400

    update context.c and taskimpl.h

--- src/taskimpl.h
+++ src/taskimpl.h
@@ -141,17 +141,7 @@ extern pid_t rfork_thread(int, void*, int(*)(void*), void*);
 #endif
 
 #if defined(__arm__)
-
-extern int getmcontext(mcontext_t *);
-extern void setmcontext(const mcontext_t *);
-/*
-int getmcontext(mcontext_t*);
-void setmcontext(const mcontext_t*);
-#define	setcontext(u)	setmcontext(&(u)->uc_mcontext)
-#define	getcontext(u)	getmcontext(&(u)->uc_mcontext)
-*/
-#define setcontext(u)    setmcontext((void *)&((u)->uc_mcontext.arm_r0))
-#define getcontext(u)    getmcontext((void *)&((u)->uc_mcontext.arm_r0))
+#include "arm-ucontext.h"
 #endif
 
 #if defined(__mips__)
